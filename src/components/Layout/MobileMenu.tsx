0f34b29e3ca0730e18d1322aa46b40d8ecdeefd0
Resvan K <muhammedresvan@gmail.com>
2024-11-23 19:33:00 +0530

    mobile menu

---
+++ src/components/Layout/MobileMenu.tsx
@@ -0,0 +1,50 @@
+import Link from "next/link";
+import Button from "../Button/Button";
+
+interface MobileMenuProps {
+    setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+export default function MobileMenu({ setOpen }: MobileMenuProps) {
+    const navLinks = [
+        { name: 'Home', url: '/' },
+        { name: 'About', url: '/about' },
+        { name: 'Services', url: '/services' },
+        { name: 'Contact Us', url: '/contact' },
+    ];
+    return (
+        <div className='p-6 bg-white absolute top-6 left-0 z-50 w-full flex flex-col justify-between md:hidden'>
+            <div>
+                <div className="flex justify-end items-center self-stretch">
+                    <button onClick={() => setOpen(false)}>
+                        <img src="/close.svg" alt="Logo" className='h-6 w-auto' />
+                    </button>
+                </div>
+                <nav>
+                    <ul className="">
+                        <ul className="hidden md:flex space-x-6 text-lg">
+                            {navLinks.map((link, idx) => (
+                                <li key={idx}>
+                                    <Link href={link.url} className="hover:text-blue-500 font-poppins">
+                                        {link.name}
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
+                        <li className="pt-4 w-full text-app-black font-poppins text-sm font-semibold pb-2 border-b">
+                            <a href="#" className='w-full'>Home</a>
+                        </li>
+                        <li className="pt-4 w-full text-app-black font-poppins text-sm font-semibold pb-2 border-b">
+                            <a href="#" className='w-full'>Services</a>
+                        </li>
+                        <li className="pt-4 w-full text-app-black font-poppins text-sm font-semibold pb-2 border-b">
+                            <a href="#" className='w-full'>About</a>
+                        </li>
+                        <li className="pt-4 w-full text-app-black font-poppins text-sm font-semibold pb-2 border-b">
+                            <a href="#" className='w-full'>Contact Us</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    )
+}
--- src/components/Layout/Nav.tsx
+++ src/components/Layout/Nav.tsx
@@ -1,5 +1,8 @@
+"use client"
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
+import MobileMenu from "./MobileMenu";
 
 export default function Nav() {
     const navLinks = [
@@ -9,22 +12,36 @@ export default function Nav() {
         { name: 'Contact Us', url: '/contact' },
     ];
 
+    const [open, setOpen] = useState(false);
+
     return (
-        <nav className="sticky bg-white top-0 z-50 flex justify-between items-center px-4 md:px-7 lg:px-14 py-3.5">
-            <div className="text-2xl font-bold">
-                <Link href="/">
-                    <Image src="/jeta-logo.svg" className="h-10 w-auto" alt="Logo" height={64} width={298} />
-                </Link>
+        <nav className="sticky bg-white top-0 z-50 px-4 md:px-7 lg:px-14 py-3.5">
+            <div className="flex justify-between items-center">
+                <div className="text-2xl font-bold">
+                    <Link href="/">
+                        <Image src="/jeta-logo.svg" className="h-10 w-auto" alt="Logo" height={64} width={298} />
+                    </Link>
+                </div>
+                <ul className="hidden md:flex space-x-6 text-lg">
+                    {navLinks.map((link, idx) => (
+                        <li key={idx}>
+                            <Link href={link.url} className="hover:text-blue-500 font-poppins">
+                                {link.name}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+                <div onClick={()=>setOpen(!open)} className="block md:hidden">
+                    <Image alt="" src={'/menu.svg'} width={24} height={24} className="h-6 w-6 object-contain object-center" />
+                </div>
             </div>
-            <ul className="hidden md:flex space-x-6 text-lg">
-                {navLinks.map((link, idx) => (
-                    <li key={idx}>
-                        <Link href={link.url} className="hover:text-blue-500 font-poppins">
-                            {link.name}
-                        </Link>
-                    </li>
-                ))}
-            </ul>
+            <div className="relative">
+                {
+                    open &&
+                    <MobileMenu setOpen={setOpen} />
+                }
+            </div>
         </nav>
+
     );
 }
